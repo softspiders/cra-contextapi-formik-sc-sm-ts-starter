d8611e77838270b3728238b693018b1a9285960b
AlexanderLapygin <AlexanderLapygin@gmail.com>
2020-03-04 19:02:13 +0300

    feat: removal of excess

--- src/index.tsx
+++
@@ -1,7 +0,0 @@
-import 'react-app-polyfill/ie11';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from "./components/App";
-import "./styles.css";
-
-ReactDOM.render(<App />, document.getElementById('root'));
